f50e86a752ba3857c76d70711be7cdf75fdae934
Varun Gandhi <git@cutcul.us>
2026-04-25 08:35:25 +0200

    feat(option): Add IsNone and Compare functions

--- common/core/option/option.go
+++ common/core/option/option.go
@@ -1,7 +1,12 @@
 // Package option provides a generic optional value type.
 package option
 
-import "github.com/typesanitizer/happygo/common/assert"
+import (
+	"cmp"
+
+	"github.com/typesanitizer/happygo/common/assert"
+	"github.com/typesanitizer/happygo/common/cmpx"
+)
 
 // Option represents a value that may or may not be present.
 type Option[T any] struct {
@@ -46,10 +51,27 @@ func (o Option[T]) IsSome() bool {
 	return o.valid
 }
 
+func (o Option[T]) IsNone() bool {
+	return !o.valid
+}
+
 // ValueOr returns the contained value if present, otherwise fallback.
 func (o Option[T]) ValueOr(fallback T) T {
 	if o.valid {
 		return o.value
 	}
 	return fallback
+}
+
+// Compare o1 o2 ensures the following ordering:
+// 1. None == None
+// 2. None < Some(v)
+// 3. Some(v1) < Some(v2) iff v1 < v2
+func Compare[T cmp.Ordered](o1 Option[T], o2 Option[T]) int {
+	v1, ok1 := o1.Get()
+	v2, ok2 := o2.Get()
+	if ok1 && ok2 {
+		return cmp.Compare(v1, v2)
+	}
+	return cmpx.CompareBool(ok1, ok2)
 }
