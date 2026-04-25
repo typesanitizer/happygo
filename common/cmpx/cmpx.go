c6607f8b6bbe499676ed95f77ad750980fbeb044
Varun Gandhi <git@cutcul.us>
2026-04-25 07:47:33 +0200

    feat(common): Add cmpx.CompareBool helper

---
+++ common/cmpx/cmpx.go
@@ -0,0 +1,15 @@
+package cmpx
+
+// CompareBool implements false < true
+func CompareBool(b1 bool, b2 bool) int {
+	if b1 {
+		if b2 {
+			return 0
+		}
+		return -1
+	}
+	if b2 {
+		return 1
+	}
+	return 0
+}
---
+++ common/cmpx/cmpx_test.go
@@ -0,0 +1,30 @@
+package cmpx_test
+
+import (
+	"testing"
+
+	"github.com/typesanitizer/happygo/common/check"
+	"github.com/typesanitizer/happygo/common/cmpx"
+)
+
+func TestCompareBool(t *testing.T) {
+	h := check.New(t)
+	h.Parallel()
+
+	type TestCase struct {
+		LHS  bool
+		RHS  bool
+		Want int
+	}
+
+	testCases := []TestCase{
+		{LHS: false, RHS: false, Want: 0},
+		{LHS: false, RHS: true, Want: 1},
+		{LHS: true, RHS: false, Want: -1},
+		{LHS: true, RHS: true, Want: 0},
+	}
+
+	for _, tc := range testCases {
+		check.AssertSame(h, tc.Want, cmpx.CompareBool(tc.LHS, tc.RHS), "comparison result")
+	}
+}
